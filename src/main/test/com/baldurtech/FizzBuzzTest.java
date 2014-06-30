ff5aeccf1ced6a026cb85138d7b6613d93aee40a
baijiayong <jiayongbai@gmail.com>
2014-06-30 11:54:05 +0800

    把每一个测试案例抽取成一个独立的方法

--- src/main/test/com/baldurtech/FizzBuzzTest.java
+++ src/main/test/com/baldurtech/FizzBuzzTest.java
@@ -6,15 +6,28 @@ public class FizzBuzzTest
     
     public static void main(String args[])
     {
+        test_1_should_be_1();
+           
+        test_2_should_be_2();
+
+        test_3_shoule_be_Fizz();
+        
+        outputTestResult();
+    }
+    public static void test_1_should_be_1()
+    {
         FizzBuzz fizzBuzz1 = new FizzBuzz();      
         asserEquals("1",fizzBuzz1.say(1));
-        
+    }
+    public static void test_2_should_be_2()
+    {
         FizzBuzz fizzBuzz2 = new FizzBuzz();
         asserEquals("2",fizzBuzz2.say(2));
-      
-        FizzBuzz fizzBuzz3 = new FizzBuzz();   
+    }
+    public static void test_3_shoule_be_Fizz()
+    {
+        FizzBuzz fizzBuzz3 = new FizzBuzz();
         asserEquals("Fizz",fizzBuzz3.say(3));
-        outputTestResult();
     }
     public static void asserEquals(String expectedResult,String actualResult)
     {
