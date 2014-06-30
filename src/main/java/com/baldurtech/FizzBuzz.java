6254f794bc18fe008f9219a5791d64b5dade352f
baijiayong <jiayongbai@gmail.com>
2014-06-30 10:10:03 +0800

    测试 2 通过

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -4,11 +4,12 @@ public class FizzBuzz
 {
     public String say(Integer num)
     {
-        return "1";
+        return String.valueOf(num);
     }
     public static void main(String args[])
     {
         FizzBuzz fizzBuzz = new FizzBuzz();
         System.out.println("1".equals(fizzBuzz.say(1)));
+        System.out.println("2".equals(fizzBuzz.say(2)));
     }
 }
