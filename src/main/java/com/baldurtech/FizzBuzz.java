73a4bd60c972e53bd594aa4ac150c7d731a3af4a
baijiayong <jiayongbai@gmail.com>
2014-06-30 10:46:36 +0800

    把测试代码的部分，独立成一个类，并修改运行测试类的run 脚本

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -10,37 +10,4 @@ public class FizzBuzz
         }
         return String.valueOf(num);
     }
-    
-    static Boolean testResult = true;
-    
-    public static void main(String args[])
-    {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String actualResult = fizzBuzz.say(1);
-        
-        asserEquals("1",fizzBuzz.say(1));
-        
-        asserEquals("2",fizzBuzz.say(2));
-        
-        asserEquals("Fizz",fizzBuzz.say(3));
-        outputTestResult();
-    }
-    public static void asserEquals(String expectedResult,String actualResult)
-    {
-       if(false ==(expectedResult.equals(actualResult)))
-        {
-            testResult = false;
-            System.out.println("Excepted '" + expectedResult + "' , but '" + actualResult + "'");
-        }
-    }
-    private static void outputTestResult()
-    {
-        if(testResult)
-        {
-            System.out.println("TEST SUCCESS");
-        }else
-        {
-            System.out.println("TEST FAILED");
-        }
-    }
 }
