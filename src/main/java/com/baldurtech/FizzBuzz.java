f0304177031eeded671b62007e9a6daf19dc507f
baijiayong <jiayongbai@gmail.com>
2014-06-30 10:21:11 +0800

    显示输出的最终结果，而不是单个输出结果

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -8,8 +8,27 @@ public class FizzBuzz
     }
     public static void main(String args[])
     {
+        Boolean testResult = true;
+        
         FizzBuzz fizzBuzz = new FizzBuzz();
-        System.out.println("1".equals(fizzBuzz.say(1)));
-        System.out.println("2".equals(fizzBuzz.say(2)));
+        String actualResult = fizzBuzz.say(1);
+        if(false ==("1".equals(fizzBuzz.say(1))))
+        {
+            testResult = false;
+            System.out.println("Excepted '1' , but '" + actualResult + "'");
+        }
+         if(false ==("2".equals(fizzBuzz.say(2))))
+        {
+            testResult = false;
+            System.out.println("Excepted '2' , but '" + actualResult + "'");
+        }
+        if(testResult)
+        {
+            System.out.println("TEST SUCCESS");
+        }else
+        {
+            System.out.println("TEST FAILED");
+        }
+        
     }
 }
