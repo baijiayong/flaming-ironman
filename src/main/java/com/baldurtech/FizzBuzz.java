8ad14ebd9edaafa01b36f66c7597daa100562cbb
baijiayong <jiayongbai@gmail.com>
2014-06-30 10:08:08 +0800

    修改代码，并且显示出测试结果

--- src/main/java/com/baldurtech/FizzBuzz.java
+++ src/main/java/com/baldurtech/FizzBuzz.java
@@ -4,11 +4,11 @@ public class FizzBuzz
 {
     public String say(Integer num)
     {
-        return null;
+        return "1";
     }
     public static void main(String args[])
     {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        System.out.println(fizzBuzz.say(1));
+        System.out.println("1".equals(fizzBuzz.say(1)));
     }
 }
