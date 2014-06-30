55b3733d3afc72640db56a2d23e92b28dc520e0b
baijiayong <jiayongbai@gmail.com>
2014-06-30 16:32:45 +0800

    把for循环中的获取以test开头的方法和调用方法的执行分离开

--- src/main/test/com/baldurtech/FizzBuzzTest.java
+++ src/main/test/com/baldurtech/FizzBuzzTest.java
@@ -1,28 +1,35 @@
 package com.baldurtech;
 
 import java.lang.reflect.Method;
+import java.util.List;
+import java.util.ArrayList;
 
 public class FizzBuzzTest
 {   
    static Boolean testResult = true;
     
     public static void main(String args[]) throws Exception
-    {    
+    {
+        List<Method> testMethods = new ArrayList<Method>();
         Method[] methods = FizzBuzzTest.class.getDeclaredMethods();
         for(Method method : methods)
         {
             if(method.getName().startsWith("test"))
             {
-                System.out.println("testing: " + method.getName());
-                Object obj = FizzBuzzTest.class.newInstance();
-                method.invoke(obj,new Object[]{});
+                testMethods.add(method);
             }
+        }
+        for(Method method : testMethods)
+        {
+            System.out.println("testing: " + method.getName());
+            Object obj = FizzBuzzTest.class.newInstance();
+            method.invoke(obj,new Object[]{});
         }        
         outputTestResult();
     }
    
     FizzBuzz fizzBuzz = new FizzBuzz();
-    
+ 
     public void test_1_should_be_1()
     {     
         asserEquals("1",fizzBuzz.say(1));
