d77cb5feef42b78885d64102c30a4b364042be93
baijiayong <jiayongbai@gmail.com>
2014-07-06 16:48:14 +0800

    添加了测试2的用例

--- src/main/test/com/baldurtech/HelloWorldTest.java
+++ src/main/test/com/baldurtech/HelloWorldTest.java
@@ -4,8 +4,12 @@ import com.baldurtech.unit.MiniatureSpiceTestCase;
 
 public class HelloWorldTest extends MiniatureSpiceTestCase
 {
-    public void test_1_shoule_equals_1()
+    public void test_1_should_equals_1()
     {
         assertEquals("1",String.valueOf(1));
+    }
+    public void test_2_should_equals_2()
+    {
+        assertEquals(2,Integer.parseInt("2"));
     }
 }
