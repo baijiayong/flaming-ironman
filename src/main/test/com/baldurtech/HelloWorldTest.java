77c3b1b65a82bdd8803d89986b39867c155c2f93
baijiayong <jiayongbai@gmail.com>
2014-07-06 16:28:44 +0800

    把 MiniatureSpiceTestCase 移动到 unit 包下

--- src/main/test/com/baldurtech/HelloWorldTest.java
+++ src/main/test/com/baldurtech/HelloWorldTest.java
@@ -1,5 +1,7 @@
 package com.baldurtech;
 
+import com.baldurtech.unit.MiniatureSpiceTestCase;
+
 public class HelloWorldTest extends MiniatureSpiceTestCase
 {
     public void test_1_shoule_equals_1()
---
+++ src/main/test/com/baldurtech/unit/MiniatureSpiceTestCase.java
@@ -0,0 +1,80 @@
+package com.baldurtech.unit;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.reflections.Reflections;
+
+public class MiniatureSpiceTestCase
+{
+    static Boolean testResult = true;
+    
+    public static void main(String args[]) throws Exception
+    {
+        
+        if(args.length > 0)
+        {
+            String testPackage = args[0];
+         
+            Reflections reflections = new Reflections(testPackage);
+            
+            Set<Class<? extends MiniatureSpiceTestCase>> allTestCase = reflections.getSubTypesOf(MiniatureSpiceTestCase.class);
+        
+            for(Class clazz: allTestCase)
+            {
+            System.out.println("Testing: " + clazz.getName());
+            runAllTest(clazz);
+            }
+            outputTestResult();
+        }
+    }
+    public static void runAllTest(Class clazz) throws Exception
+    {
+        for(Method method : getAllTestMethod(clazz))
+        {
+            System.out.println("testing: " + method.getName());
+            try{
+                Object obj = clazz.newInstance();
+                method.invoke(obj,new Object[]{});
+            }catch(Exception e)
+            {
+                testResult = false;
+                System.out.println("Unexcepted exception!");
+                e.printStackTrace();
+            }
+        }      
+    }
+    public static List<Method> getAllTestMethod(Class clazz) throws Exception
+    {
+        List<Method> testMethods = new ArrayList<Method>();
+        Method[] methods = clazz.getDeclaredMethods();
+        for(Method method : methods)
+        {
+            if(method.getName().startsWith("test"))
+            {
+                testMethods.add(method);
+            }
+        }
+        return testMethods;
+    }
+    public static void assertEquals(String expectedResult,String actualResult)
+    {
+       if(false ==(expectedResult.equals(actualResult)))
+        {
+            testResult = false;
+            System.out.println("Excepted '" + expectedResult + "' , but '" + actualResult + "'");
+        }
+    }
+    private static void outputTestResult()
+    {
+        if(testResult)
+        {
+            System.out.println("TEST SUCCESS");
+        }else
+        {
+            System.out.println("TEST FAILED");
+        }
+    }
+}
